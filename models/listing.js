520971a7a19ce1c8ab039f83f349ca604fb3f99f
Sujal <sujaldhote1@gmail.com>
2024-07-28 16:39:46 +0530

    new changes

--- models/listing.js
+++ models/listing.js
@@ -12,8 +12,8 @@ const listingSchema = new Schema({
     },
     image: {
         type: String,
-        default: "https://unsplash.com/photos/a-green-and-blue-sky-with-a-lot-of-snow-4gIJkfVcnWM",
-        set: (v) => v === "" ? "https://unsplash.com/photos/a-green-and-blue-sky-with-a-lot-of-snow-4gIJkfVcnWM" : v,
+        default: "https://plus.unsplash.com/premium_photo-1675805016043-0166fc57ad01?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1675805016043-0166fc57ad01?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
     },
     price: {
         type: Number,
--- schema.js
+++ schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const listingJoiSchema = Joi.object({
     title: Joi.string().required(),
     description: Joi.string().default(""),
-    image: Joi.string().default("https://unsplash.com/photos/a-green-and-blue-sky-with-a-lot-of-snow-4gIJkfVcnWM"),
+    image: Joi.string().allow("",null),
     price: Joi.number().default(0).min(0),
     location: Joi.string().default(""),
     country: Joi.string().default("")
