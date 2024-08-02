a0bce25248d03caac6616227a34d7d3fad6f9aa6
Sujal <sujaldhote1@gmail.com>
2024-08-02 15:30:47 +0530

    new changes added

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ the code is doing: */
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const ejsMate = require("ejs-mate");
-const wrapasync = require("./utils/wrapasync.js");
 const ExpressError = require("./utils/Expresserror.js");
-const { listingJoiSchema, reviewSchema } = require("./schema.js");
 const methodOverride = require("method-override");
-const Review = require("./models/review.js");
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 
 // Setting up EJS as the view engine
@@ -40,109 +38,10 @@ app.get("/", (req, res) => {
     res.send("root is working");
 });
 
-const validateListing = (req, res, next) => {
-    console.log(req.body);
-    let { error } = listingJoiSchema.validate(req.body.listing);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
 
 
-const validateReview = (req, res, next) => {
-    console.log(req.body);
-    let { error } = reviewSchema.validate(req.body.listing);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
-
-app.get("/listings", wrapasync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-}));
-
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-});
-
-app.get("/listings/:id", wrapasync(async (req, res) => {
-    const { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    if (!listing) {
-        throw new ExpressError(404, "Listing not found");
-    }
-    res.render("listings/show.ejs", { listing });
-}));
-
-app.post("/listings", validateListing, wrapasync(async (req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-}));
-
-app.get("/listings/:id/edit", wrapasync(async (req, res) => {
-    const { id } = req.params;
-    const listing = await Listing.findById(id);
-    if (!listing) {
-        throw new ExpressError(404, "Listing not found");
-    }
-    res.render("listings/edit.ejs", { listing });
-}));
-
-app.put("/listings/:id", validateListing, wrapasync(async (req, res) => {
-    const { id } = req.params;
-    const updatedListing = await Listing.findByIdAndUpdate(id, { ...req.body.listing }, { new: true });
-    if (!updatedListing) {
-        throw new ExpressError(404, "Listing not found");
-    }
-    res.redirect(`/listings/${id}`);
-}));
-1
-app.delete("/listings/:id", wrapasync(async (req, res) => {
-    const { id } = req.params;
-    const deletedListing = await Listing.findByIdAndDelete(id);
-    if (!deletedListing) {
-        throw new ExpressError(404, "Listing not found");
-    }
-    res.redirect("/listings");
-}));
-
-
-// reviews
-
-app.post("/listings/:id/reviews", validateReview, wrapasync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    // console.log("new review saved");
-    // res.send("new review saved");
-
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-
-
-// Delete review route
-
-app.delete("/listings/:id/reviews/:reviewId", wrapasync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-}))
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
 
 // 404 Route Handler
---
+++ routes/listing.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const wrapasync = require("../utils/wrapasync.js");
+const { listingJoiSchema } = require("../schema.js");
+const ExpressError = require("../utils/Expresserror.js");
+const Listing = require("../models/listing.js");
+
+const validateListing = (req, res, next) => {
+    console.log(req.body);
+    let { error } = listingJoiSchema.validate(req.body.listing);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+router.get("/", wrapasync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+}));
+
+router.get("/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+router.get("/:id", wrapasync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    if (!listing) {
+        throw new ExpressError(404, "Listing not found");
+    }
+    res.render("listings/show.ejs", { listing });
+}));
+
+
+
+router.post("/", validateListing, wrapasync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+}));
+
+router.get("/:id/edit", wrapasync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+        throw new ExpressError(404, "Listing not found");
+    }
+    res.render("../views/listings/edit.ejs", { listing });
+}));
+
+router.put("/:id", validateListing, wrapasync(async (req, res) => {
+    const { id } = req.params;
+    const updatedListing = await Listing.findByIdAndUpdate(id, { ...req.body.listing }, { new: true });
+    if (!updatedListing) {
+        throw new ExpressError(404, "Listing not found");
+    }
+    res.redirect(`/listings/${id}`);
+}));
+1
+router.delete("/:id", wrapasync(async (req, res) => {
+    const { id } = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id);
+    if (!deletedListing) {
+        throw new ExpressError(404, "Listing not found");
+    }
+    res.redirect("/listings");
+}));
+
+module.exports = router;
