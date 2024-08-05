17364a6d497b4beffe49fd0e29e7718dd7fbc426
Sujal <sujaldhote1@gmail.com>
2024-08-05 14:21:32 +0530

    new files added

--- app.js
+++ app.js
@@ -7,10 +7,17 @@ const path = require("path");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/Expresserror.js");
 const methodOverride = require("method-override");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
 
@@ -58,7 +65,16 @@ app.use(session(sessionOptions));
 app.use(flash());
 
 
-app.use((req,res,next)=>{
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     // console.log(res.locals.success);
@@ -66,8 +82,20 @@ app.use((req,res,next)=>{
 })
 
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "sujal"
+//     });
+
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// })
+
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 
 // 404 Route Handler
